73c56154a0f807d16ad0da3251a717e9545d0147
Aryan Kashyap <kashyaparyan093@gmail.com>
2026-01-26 05:21:13 +0000

    Update page.tsx

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
     <>
       <SectionNavigation />
       <HeroSection />
-      <HowItWorksSection />
       <FeatureSection />
+      <HowItWorksSection />
       <PricingSection />
       <Footer />
     </>
